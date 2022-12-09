2cce38064a754c861f5aa77b0c38361731d66720
Pooja Whaval <whavalpooja@gmail.com>
2022-12-09 11:55:25 +0530

    added redux persist

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { store } from './src/redux/store'
 import Toast from 'react-native-toast-message';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 import { Provider } from 'react-redux'
 import Routes from './src/routes';
 import { toastConfig } from './src/components/toast';
 
+let persistor = persistStore(store);
+
 const App = () => {
   return (
     <Provider store={store}>
-      <Routes />
-      <Toast config={toastConfig} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes />
+        <Toast config={toastConfig} />
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/user';
+import { persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import thunk from 'redux-thunk'
+
+const reducers = combineReducers({
+    userDetails: userReducer
+})
+
+const persistConfig = {
+  key: 'root',
+  storage:AsyncStorage,
+  whitelist: ['userDetails']
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    userDetails: userReducer
-  },
-})
+  reducer: persistedReducer,
+  middleware: [thunk]
+});
